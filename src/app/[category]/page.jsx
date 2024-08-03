a3766a51533f3bc8824b877e9b40fcf4567cb89d
mdyiaz <mdyiaz@gmail.com>
2024-08-03 16:46:04 +0600

    assets meta

--- src/app/[category]/page.jsx
+++ src/app/[category]/page.jsx
@@ -2,20 +2,42 @@ import SubCategory from '@/components/SubCategory/SubCategory/SubCategory';
 import getData from '@/utils/getData';
 import React from 'react';
 
-const Page = async ({params}) => {
+
+// export const metadata = {
+//     title: `${applicationSettings?.data?.site_name}`,
+//     description: `${applicationSettings?.data?.site_description}`,
+// };
+
+export async function generateMetadata({ params }) {
+    const { category } = params;
+    const categoryId = category?.split("-").slice(-1);
+
+    console.log("looking ", categoryId);
+
+    const metaSettings = await getData(`categories/${categoryId}`); // Replace with actual call to fetch site settings
+    const metaTitle = metaSettings?.data?.meta_title;
+    const metaDescription = metaSettings?.data?.meta_description;
+
+    return {
+        title: `${metaTitle}`,
+        description: `${metaDescription}`,
+    };
+}
+
+const Page = async ({ params }) => {
 
     const { category } = params;
 
     const categoryId = category?.split("-").slice(-1);
-   
+
 
     const subCategoriesByCategoryId = await getData(`categories/${categoryId}`);
-  
+
 
 
     return (
         <div>
-            <SubCategory subCategoriesByCategoryId= {subCategoriesByCategoryId}   />
+            <SubCategory subCategoriesByCategoryId={subCategoriesByCategoryId} />
         </div>
     );
 };
