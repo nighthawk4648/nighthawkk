d4f0748888e91074d9ce483024745e3704fe0622
Baharul Islam Shuvo <baharulislam952@gmail.com>
2025-03-17 01:28:04 +0600

    minor changes

--- src/app/license/page.jsx
+++ src/app/license/page.jsx
@@ -3,15 +3,8 @@ import React from 'react';
 
 export const metadata = {
     title: 'License',
-<<<<<<< HEAD
     description: 'CCO means absolute freedom',
   };
-  
-
-=======
-    description: 'CCO means absolute freedom',
-  };
->>>>>>> 61538d4e56811bc55c0254461b30b9946c7458e6
 const License = async () => {
 
 
