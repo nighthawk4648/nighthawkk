bcef5462ad9b4c2117df4da4475129b85e5d36ed
Baharul Islam Shuvo <baharulislam952@gmail.com>
2025-03-16 16:32:56 +0600

    menu bar updated & make drawer to every sub category element

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -1,3 +1,156 @@
+// 'use client';
+// import { useState } from 'react';
+
+// import Image from 'next/image';
+// import Link from 'next/link';
+// import { AiOutlineMenuFold } from 'react-icons/ai';
+// import { IoCloseCircleOutline, IoSearchSharp } from 'react-icons/io5';
+// import { MdDashboard } from 'react-icons/md';
+// import navLogo from '../../../../public/assets/navbar/navbar_logo.png';
+// import slugify from '@/utils/slugify';
+// import MenuBarForMobileScreen from './MenuBarForMobileScreen';
+// import SearchForMobileScreen from './SearchForMobileScreen';
+// import SearchForDesktopScreen from './SearchForDesktopScreen';
+
+// const Navbar = ({ categories, footerPages }) => {
+// 	const [isOpen, setIsOpen] = useState(false);
+
+// 	const toggleNavbar = () => {
+// 		setIsOpen(!isOpen);
+// 	};
+
+
+// 	// Find the footer page with id 5
+// 	const footerPage = footerPages?.find(page => page.id === 5);
+
+
+
+// 	return (
+// 		<div className="relative">
+// 			<nav className=" bg-black  md:px-5 px-1  shadow-sm  ">
+// 				<div className=" px-4">
+// 					<div className="flex justify-between items-center ">
+// 						<div className="flex  items-center gap-5">
+// 							<p className="font-bold text-white"><Link href="/">Sketchshaper</Link></p>
+// 						</div>
+
+// 						<div className="flex items-center">
+// 							<div className="hidden md:block">
+// 								<div className="flex items-center  gap-5">
+// 									<div>
+
+// 										<SearchForDesktopScreen />
+
+// 									</div>
+
+// 									<p className="font-semibold text-white"><Link href='https://blog.sketchshaper.com/'>BLOG</Link> </p>
+
+// 									<p className="font-semibold text-white"><Link href="/">HOME</Link></p>
+// 									<div className="group py-3">
+// 										<p className="font-semibold group cursor-pointer text-white">MENU</p>
+
+// 										<div className="bg-secondary text-white fixed w-full right-0 top-[50px] hidden group-hover:flex gap-5 p-4  h-4/5 overflow-y-auto ">
+// 											<div className=" flex justify-between w-full ">
+// 												{
+// 													categories?.map((category) => (
+// 														<div className='px-4'>
+// 															<div className=' mb-3 font-semibold uppercase'>
+
+// 																<Link href={`/${slugify(category?.name)}-${category?.id}`}><p>{category?.name}</p></Link>
+// 															</div>
+// 															<div>
+// 																{
+// 																	category?.sub_categories?.map((subCategory) => (
+// 																		<div>
+// 																			<div className=' mt-2 font-semibold'>
+// 																				<Link
+// 																					href={`/${slugify(category?.name)}-${category?.id}/${subCategory?.name}-${subCategory?.id}`}
+// 																				>
+// 																					<p>{subCategory?.name}</p>
+// 																				</Link>
+// 																			</div>
+
+// 																			<div>
+// 																				{
+// 																					subCategory?.assets?.map((asset) => (
+// 																						<div className='text-sm mt-1 '>
+// 																							<Link href={`/${slugify(category?.name)}/${slugify(subCategory?.name)}/${slugify(asset?.name)}-${asset?.id}`}><p>{asset?.name}</p></Link>
+
+// 																						</div>
+// 																					))
+// 																				}
+// 																			</div>
+// 																		</div>
+// 																	))
+// 																}
+// 															</div>
+// 														</div>
+// 													))
+// 												}
+
+// 											</div>
+// 										</div>
+// 									</div>
+// 								</div>
+// 							</div>
+// 							<div className="md:hidden">
+// 								<div className='flex gap-3 items-center mt-2'>
+// 									<SearchForMobileScreen />
+// 									<button onClick={toggleNavbar} className=" font-medium ">
+// 										<AiOutlineMenuFold className="md:text-2xl text-xl text-white " />
+// 									</button>
+// 								</div>
+// 							</div>
+// 						</div>
+// 					</div>
+// 				</div>
+
+// 				{/* Mobile menu */}
+// 				<div className={`md:hidden ${isOpen ? 'block' : 'hidden'}`}>
+// 					<div className="fixed inset-0 z-50 bg-gray-400 opacity-75"></div>
+// 					<div
+// 						className={`fixed inset-y-0 left-0 z-50 w-4/5 bg-white transform transition-transform ease-in-out duration-300 ${isOpen ? 'translate-x-0' : '-translate-x-full'
+// 							}`}
+// 					>
+// 						<div className="flex items-center justify-end h-16 px-4">
+// 							<button onClick={toggleNavbar} className="   font-medium">
+// 								<IoCloseCircleOutline className="text-3xl text-gray-500" />
+// 							</button>
+// 						</div>
+// 						<div className=" h-full  mt-3">
+// 							<div className="flex gap-3 items-center mb-7 border-b border-gray-200 shadow-sm  px-3 py-3">
+// 								{/* <MdOutlineAccountCircle className="text-4xl" /> */}
+// 								<Link href="/">
+// 									{/* <Image
+// 										src={navLogo}
+// 										height={200}
+// 										width={500}
+// 										className="h-auto w-28"
+// 										alt="nighthawk"
+// 									></Image> */}
+
+// 									<p className='text-2xl font-semibold'>Sketch Shaper</p>
+// 								</Link>
+// 							</div>
+
+// 							<div
+// 								className=" "
+// 							// onClick={toggleNavbar}
+// 							>
+
+// 								<MenuBarForMobileScreen categories={categories} />
+
+// 							</div>
+// 						</div>
+// 					</div>
+// 				</div>
+// 			</nav>
+// 		</div>
+// 	);
+// };
+
+// export default Navbar;
+
 
 // 4th attempt
 
@@ -150,22 +303,37 @@ const Navbar = ({ categories, footerPages }) => {
 				<div className={`md:hidden ${isOpen ? 'block' : 'hidden'}`}>
 					<div className="fixed inset-0 z-50 bg-gray-400 opacity-75"></div>
 					<div
-						className={`fixed inset-y-0 left-0 z-50 w-4/5 bg-white transform transition-transform ease-in-out duration-300 ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}
+						className={`fixed inset-y-0 left-0 z-50 w-4/5 bg-white transform transition-transform ease-in-out duration-300 ${isOpen ? 'translate-x-0' : '-translate-x-full'
+							}`}
 					>
 						<div className="flex items-center justify-end h-16 px-4">
-							<button onClick={toggleNavbar} className="font-medium">
+							<button onClick={toggleNavbar} className="   font-medium">
 								<IoCloseCircleOutline className="text-3xl text-gray-500" />
 							</button>
 						</div>
-						<div className="h-full mt-3">
-							<div className="flex gap-3 items-center mb-7 border-b border-gray-200 shadow-sm px-3 py-3">
+						<div className=" h-full  mt-3">
+							<div className="flex gap-3 items-center mb-7 border-b border-gray-200 shadow-sm  px-3 py-3">
+								{/* <MdOutlineAccountCircle className="text-4xl" /> */}
 								<Link href="/">
+									{/* <Image
+										src={navLogo}
+										height={200}
+										width={500}
+										className="h-auto w-28"
+										alt="nighthawk"
+									></Image> */}
+
 									<p className='text-2xl font-semibold'>Sketch Shaper</p>
 								</Link>
 							</div>
 
-							<div>
+							<div
+								className=" "
+							// onClick={toggleNavbar}
+							>
+
 								<MenuBarForMobileScreen categories={categories} />
+
 							</div>
 						</div>
 					</div>
