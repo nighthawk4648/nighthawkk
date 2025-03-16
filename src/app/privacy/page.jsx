24210baeb5b67ae179f666174493a3f69a61f620
nighthawk4648 <nighthawk4648@gmail.com>
2025-03-17 01:33:47 +0600

    update

--- src/app/privacy/page.jsx
+++ src/app/privacy/page.jsx
@@ -5,15 +5,9 @@ import React from 'react';
 
 export const metadata = {
     title: 'Privacy Policy',
-<<<<<<< HEAD
+    
     description: 'Read out privacy policy include third party services',
-  };
-  
-
-=======
-    description: 'Read out privacy policy include third party services',
-  };
->>>>>>> 61538d4e56811bc55c0254461b30b9946c7458e6
+};
 const privacy = async () => {
 
 
--- src/app/terms-and-conditions/page.jsx
+++ src/app/terms-and-conditions/page.jsx
@@ -4,14 +4,9 @@ import React from 'react';
 
 export const metadata = {
     title: 'Terms and Conditions',
-<<<<<<< HEAD
-    description: '',
-  };
-  
-=======
+    
     description: '',
-  };
->>>>>>> 61538d4e56811bc55c0254461b30b9946c7458e6
+ };
 
 const TermsAndCondition = async () => {
 
