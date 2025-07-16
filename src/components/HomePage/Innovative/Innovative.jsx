e2540936c99f5dba155dc73ef4c70d00654c9875
nighthawk4648 <nighthawk4648@gmail.com>
2025-07-16 14:57:24 +0600

    solve responsive

--- src/components/HomePage/Innovative/Innovative.jsx
+++ src/components/HomePage/Innovative/Innovative.jsx
@@ -1,38 +1,48 @@
+import { getOptimizedImageUrl } from '@/utils/cloudinary';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-import { AiFillPlusCircle } from 'react-icons/ai';
+import { FaCreativeCommonsSamplingPlus } from "react-icons/fa6";
 
 const Innovative = ({ innovatives }) => {
-    return (
+   
+    const getOriginalImageUrl = (imagePath) => {
+        return `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL_FOR_IMAGE}${imagePath}`;
+    }; 
+   
+  return (
+  <div>
+    {innovatives?.map((innovative, index) => (
+      <div key={index} className='relative'>
         <div className='relative'>
-            <div className='relative'>
-                <Image
-                    src={ innovatives?.bgImg ?  process.env.NEXT_PUBLIC_BACKEND_BASE_URL_FOR_IMAGE + category.image : 'https://i.ibb.co/0V8cTj0R/MF-M1.jpg' }
-                    height={500}
-                    width={500}
-                    alt={innovatives.title}
-                    className='w-full h-[200px] md:h-[500px]'
-                />
-            </div>
-            {/* Adjusted icon positioning */}
-            <div className='absolute right-[180px] top-[150px] md:top-1/2 '>
-                <Link href={`${innovatives?.urlOne ? innovatives?.urlOne : '/'}`} ><AiFillPlusCircle className='text-orange-700 text-2xl md:text-4xl' /></Link>
-            </div>
-
-            <div className='absolute right-[100px] md:right-[200px] lg:right-[500px] top-1/2 md:top-3/4 '>
-                <Link  href={`${innovatives?.urlTwo ? innovatives?.urlTwo : '/'}`} ><AiFillPlusCircle className='text-orange-700 text-2xl md:text-4xl' /></Link>
-            </div>
-
-            <div className='absolute left-[40px] md:left-[100px] lg:left-[130px] top-[60px] md:top-[170px] '>
-                <Link href={`${innovatives?.urlThree ? innovatives?.urlThree : '/'}`} ><AiFillPlusCircle className='text-orange-700 text-2xl md:text-4xl' /></Link>
-            </div>
-
-            <div className='absolute left-[100px] md:left-[300px] top-3/4 '>
-                <Link href={`${ innovatives?.urlFour ? innovatives?.urlFour : '/'}`}><AiFillPlusCircle className='text-orange-700 text-2xl md:text-4xl' /></Link>
-            </div>
+          <Image
+            src={getOptimizedImageUrl(getOriginalImageUrl(innovative?.bgImg))}
+            height={1600}
+            width={1600}
+            alt={innovative.title}
+            className='w-full h-[200px] md:h-[500px]'
+          />
         </div>
-    );
+
+        {/* Icon positions */}
+        {[
+          { url: innovative?.urlOne, position: 'absolute right-[180px] top-[150px] md:top-1/2' },
+          { url: innovative?.urlTwo, position: 'absolute right-[100px] md:right-[200px] lg:right-[500px] top-1/2 md:top-3/4' },
+          { url: innovative?.urlThree, position: 'absolute left-[40px] md:left-[100px] lg:left-[130px] top-[60px] md:top-[170px]' },
+          { url: innovative?.urlFour, position: 'absolute left-[100px] md:left-[300px] top-3/4' },
+        ].map((item, i) => (
+          <div key={i} className={item.position}>
+            <Link href={item.url || '/'}>
+              <div className='relative flex items-center'>
+                <FaCreativeCommonsSamplingPlus className='text-black text-2xl md:text-4xl heartbeat-hover' />
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
+    ))}
+  </div>
+);
 };
 
 export default Innovative;
